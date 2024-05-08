b13a2eb8a69018add623bedca38ed12d5bde1157
yl907 <yluwane@hotmail.com>
2024-05-08 17:40:44 +0800

    finish "Building your First App"

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('Hello from Electron 👋');
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// Quit the app when all windows are closed (Windows & Linux) (not macOS which is darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
